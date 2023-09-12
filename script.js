0f0b8d0463e97c90cedd75e8a6d77cc748a741ea
programmingwithVee <basseyvera018@gmail.com>
2023-09-12 12:50:31 +0100

    task one

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//     const today = new Date();
-//     const currentDay = daysOfWeek[today.getUTCDay()];
-//     const currentUTCTime = today.getTime();
-
-//     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day: ${currentDay}`;
-//     document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${currentUTCTime}`;
-// });
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-//     function updateCurrentTime() {
-//         const today = new Date();
-//         const currentDay = daysOfWeek[today.getUTCDay()];
-//         const currentUTCTime = today.getTime();
-
-//         document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `${currentDay}`;
-//         document.querySelector('[data-testid="currentUTCTime"]').textContent = `${currentUTCTime}`;
-//     }
-
-//     updateCurrentTime();
-//     setInterval(updateCurrentTime, 1000);
-// });
-
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // Get the current date
@@ -41,18 +15,13 @@ document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `${d
 // console.log("Today is " + dayOfWeekName);
 
 
-const specificDate = new Date();
+function displayCurrentUTCTime() {
+    setInterval(function () {
+        const currentTimeInMilliseconds = new Date().getTime();
+        document.querySelector('[data-testid="currentUTCTime"]').textContent = `${currentTimeInMilliseconds}`;
+    }, 1000); // Update every 1000 milliseconds (1 second)
+}
 
-
-const year = specificDate.getUTCFullYear();
-const month = specificDate.getUTCMonth() + 1;
-const day = specificDate.getUTCDate();
-const hours = specificDate.getUTCHours();
-const minutes = specificDate.getUTCMinutes();
-const seconds = specificDate.getUTCSeconds();
-const milliseconds = specificDate.getUTCMilliseconds();
-
-// Display the components
-document.querySelector('[data-testid="currentUTCTime"]').textContent=`${year}-${month}-${day} T${hours} : ${minutes} : ${seconds}.${milliseconds}Z`;
-
+// Start displaying the current UTC time
+displayCurrentUTCTime();
 
